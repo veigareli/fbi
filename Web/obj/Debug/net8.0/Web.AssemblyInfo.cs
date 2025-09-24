75bb2d0d631a04994e44e00403779582b07dfcc5
veigareli <veigareli@gmail.com>
2025-09-24 14:08:14 +0000

    Docker fix

--- Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8898289db57450fe4d74115609ebc4cba58a95f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c12bfee7295f480e1174b5511f529fb0a5aace")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
